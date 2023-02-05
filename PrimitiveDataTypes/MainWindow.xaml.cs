c2caee714af3592524a398f9e37da493312c650d
Xabierr78 <116910820+Xabierr78@users.noreply.github.com>
2023-02-05 19:30:53 +0100

    Agregar archivos de proyecto.

---
+++ PrimitiveDataTypes/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimitiveDataTypes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void typeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem selectedType = (type.SelectedItem as ListBoxItem);
+            switch (selectedType.Content.ToString())
+            {
+                case "int":
+                    showIntValue(); //acabar
+                    break;
+                case "long":
+                    showLongValue(); // acabar
+                    break;
+                case "float":
+                    showFloatValue(); // acabar
+                    break;
+                case "double":
+                    showDoubleValue(); //acabar
+                    break;
+                case "decimal":
+                    showDecimalValue();
+                    break;
+                case "string":
+                    showStringValue();
+                    break;
+                case "char":
+                    showCharValue();
+                    break;
+                case "bool":
+                    showBoolValue(); //acabar
+                    break;
+            }
+        }
+
+        private void showIntValue()
+        {
+            int intNum;
+            intNum = 24;             
+            value.Text = intNum.ToString();
+        }
+
+        private void showLongValue()
+        {
+            long longVar;
+            longVar = 42L;
+            value.Text = longVar.ToString();
+        }
+
+        private void showFloatValue()
+        {
+            float floatVar;
+            floatVar = 0.42F;
+            value.Text = floatVar.ToString();
+        }
+
+        private void showDoubleValue()
+        {
+            double doubleNum;
+            doubleNum = 2.45d; 
+            value.Text = doubleNum.ToString();
+        }
+
+        private void showDecimalValue()
+        {
+            decimal decimalVar;
+            decimalVar = 0.42M;
+            value.Text = decimalVar.ToString();
+        }
+
+        private void showStringValue()
+        {
+            string stringVar;
+            stringVar = "forty two";
+            value.Text = stringVar; 
+        }
+
+        private void showCharValue()
+        {
+            char charVar;
+            charVar = 'x';
+            value.Text = charVar.ToString();
+        }
+
+        private void showBoolValue()
+        {
+            bool boolVar;
+            boolVar = true;
+            value.Text = "true or false";
+        }
+
+        private void quitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
